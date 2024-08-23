86cedf38f7fb55c3246f1755d5351458d77d257c
Dongsu Park <dpark@linux.microsoft.com>
2024-08-23 12:11:34 +0200

    libazureinit: split get_http_response_payload into a separate function
    Split get_http_response_payload into a separate function so that it can
    be used later by other tests.

--- libazureinit/src/imds.rs
+++ libazureinit/src/imds.rs
@@ -244,6 +244,22 @@ mod tests {
         assert!(os_profile.is_err_and(|err| err.is_data()));
     }
 
+    // Returns expected HTTP response for the given status code and body string.
+    async fn get_http_response_payload(
+        statuscode: &StatusCode,
+        body_str: &str,
+    ) -> String {
+        // Reply message includes the whole body in case of OK, otherwise empty data.
+        let res = match statuscode {
+            &StatusCode::OK => format!("HTTP/1.1 {} {}\r\nContent-Type: application/json\r\nContent-Length: {}\r\n\r\n{}", statuscode.as_u16(), statuscode.to_string(), body_str.len(), body_str.to_string()),
+            _ => {
+                format!("HTTP/1.1 {} {}\r\n\r\n", statuscode.as_u16(), statuscode.to_string())
+            }
+        };
+
+        res
+    }
+
     // Runs a test around sending via imds::query() with a given statuscode.
     async fn run_imds_query_retry(statuscode: &StatusCode) -> bool {
         const IMDS_HTTP_TOTAL_TIMEOUT_SEC: u64 = 5 * 60;
@@ -254,21 +270,14 @@ mod tests {
         let user_agent =
             header::HeaderValue::from_str("azure-init test").unwrap();
 
-        // Run a local test server that replies with simple test data.
+        let ok_payload =
+            get_http_response_payload(statuscode, BODY_CONTENTS).await;
         let serverlistener = TcpListener::bind("127.0.0.1:0").await.unwrap();
         let addr = serverlistener.local_addr().unwrap();
 
-        // Reply message includes the whole body in case of OK, otherwise empty data.
-        let ok_body = match statuscode {
-            &StatusCode::OK => format!("HTTP/1.1 {} {}\r\nContent-Type: application/json\r\nContent-Length: {}\r\n\r\n{}", statuscode.as_u16(), statuscode.to_string(), BODY_CONTENTS.len(), BODY_CONTENTS.to_string()),
-            _ => {
-                format!("HTTP/1.1 {} {}\r\n\r\n", statuscode.as_u16(), statuscode.to_string())
-            }
-        };
-
         tokio::spawn(async move {
             let (mut serverstream, _) = serverlistener.accept().await.unwrap();
-            serverstream.write_all(ok_body.as_bytes()).await.unwrap();
+            serverstream.write_all(ok_payload.as_bytes()).await.unwrap();
         });
 
         // Advance time to 5 minutes later, to prevent tests from being blocked
